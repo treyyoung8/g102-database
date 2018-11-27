26094254cc99dfd35b0c65c740b38d1a1aeca967
= <treyyoung8@gmail.com>
2018-11-27 10:24:43 -0700

    added knex

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
+
+const data = [
+    {
+        name: 'Trey',
+        fave_animal: 'https://images-na.ssl-images-amazon.com/images/I/61irQrNjgnL._SY679_.jpg',
+        previous_occupation: 'General Manager',
+        hometown_lat: 37.2638,
+        hometown_long: 122.0230,
+        useless_superpower: 'super deaf'
+    }
+]
 
 app.get('/', (req, res, next) => {
     res.send('aye mang')
